afb6090f87b9e6e8d1b4dabc757dc082d74ccd1e
ThomasDickson <thomas.dickson13@outlook.com>
2022-07-26 21:34:46 +1000

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 //import logo from './logo.svg'; //use similar to import images/videos
 import './App.css';
-import navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Gallery from './components/pages/gallery';
-import Home from './components/pages/home'
-import Shop from './components/pages/shop'
+import Gallery from './components/pages/Gallery';
+import Home from './components/pages/Home'
+import Shop from './components/pages/Shop'
 
 function App() {
   return (
     <Router>
-      <navbar/>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/gallery" element={<Gallery/>} />
