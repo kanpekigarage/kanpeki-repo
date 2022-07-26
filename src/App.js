330116f3a2494a8b3ee98a4a6517130af0e9fbea
kanpekigarage <kanpekigarage@gmail.com>
2022-07-26 21:20:26 +1000

    fix

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@
 import './App.css';
 import navbar from './components/navbar';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/pages/gallery';
+import Booking from './components/pages/home'
+import Account from './components/pages/shop'
 
 function App() {
   return (
     <Router>
       <navbar/>
       <Routes>
-        
-      <Routes/>
-    <Router/>
+        <Route path="/" element={<Home/>} />
+        <Route path="/gallery" element={<Gallery/>} />
+        <Route path="/shop//*" element={<Shop/>} />  
+      </Routes>
+    </Router>
   );
 }
 
