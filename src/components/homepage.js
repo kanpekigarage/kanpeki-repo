dbc558401f7ed68493542821d26b6da1c6cde6ce
kanpekigarage <kanpekigarage@gmail.com>
2022-08-30 23:18:13 +1000

    homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import homepageCivic from '../assets/homepage.png'
+import './homepage.css';
+import hoodie from '../assets/hoodie.jpg'
+import { Link } from 'react-router-dom';
 import { Box, 
          Card, 
          CardContent, 
          CardMedia,
          Grid,
+         Button,
          Typography } from '@mui/material'
-         import './homepage.css';
+         
 
+         
 function homepage() {
   return(
     <Box
@@ -26,120 +32,144 @@ function homepage() {
           spacing={4}
         >
           <Grid item xs={12} >
-            <Typography variant="h4" color="#2796FD">
-              <b>Choose your adventure. </b>
+            <Typography variant="h4" color="#384145">
+              <b>About Kanpeki Garage</b>
             </Typography>
             <Typography marginTop="12px" variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris laoreet lectus tincidunt, dapibus enim ut, egestas nunc. Pellentesque facilisis venenatis porta. Aliquam velit ex, dignissim quis nunc ac, semper ullamcorper leo.
+            Kanpeki Garage was started as a passion project in 2022 to share our interest in cars and clothing. We aim to share quality media of cars, create a community that appreiate cars and produce quality clothing.
             </Typography>
+            <img src={homepageCivic} className="homepagecivic nodrag" alt='homeimg' />
           </Grid>
-          <Grid item xs={6}>
-            <Card >
-              <CardMedia
-                component="img"
-                height="250"
-                image="https://static.standard.co.uk/s3fs-public/thumbnails/image/2018/01/25/16/shutterstock-603577616.jpg?width=968"
-                alt="kangaroo on australian beach"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Queensland, Australia
-                </Typography>
-                <Typography variant="body1">
-                  Bathe in the Australian sun on the gorgeous beaches of Surfers Paradise or enjoy the high-adrenaline thrills of Warner Brothers' Movie World.
-                </Typography>
-                <br/>
-                <Typography color="#2796FD" variant="body1">
-                  <b>Flights from $129</b>
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={6} >
+          
+          <Grid item xs={4} className="gallerydesk">
             <Card>
               <CardMedia
                 component="img"
-                height="250"
-                image="https://imageio.forbes.com/specials-images/dam/imageserve/1139828965/960x0.jpg?fit=bounds&format=jpg&width=960"
-                alt="Bo Kaap neighborhood in Cape Town, South Africa"
+                height="350"
+                image={require('../assets/gallery.jpg')}
+                alt="Gallery"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                  Cape Town, South Africa
+                  Gallery
                 </Typography>
                 <Typography variant="body1">
-                  Explore Cape Town's vibrantly colourful neighborhoods and turquoise waters. Embark on an cultural and culinary adventure of Cape Town's endless beauty.
+                  Our gallery contains collections of images of cars that we have shot to display every aspect of people's vechicles.
                 </Typography>
-                <br />
-                <Typography color="#2796FD" variant="body1">
-                  <b>Flights from $689</b>
+                <Typography color="#3584e2" variant="body1" component={Link} to="/gallery">
+                  <b>Click Here To Visit The Gallery</b>
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={4} >
+          <Grid item xs={4} className="shopdesk">
             <Card>
               <CardMedia
                 component="img"
-                height="250"
-                image="https://purewows3.imgix.net/images/articles/2016_03/NY_Street_List5.jpg?auto=format,compress&cs=strip"
-                alt="new york city street"
+                height="350"
+                image={require('../assets/hoodie.jpg')}
+                alt="Shop"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                  New York, U.S.A
+                  Shop
                 </Typography>
                 <Typography variant="body1">
-                  New York subways, rats, pizza it has it all this life style could be all yours for the low price of $1247.
+                  Our shop contains high quality clothing, stickers and accessories. 
                 </Typography>
-                <Typography color="#2796FD" variant="body2">
-                  <b>Flights from $1247</b>
+                <Typography color="#3584e2" variant="body1" component={Link} to="/shop">
+                  <b>Click Here To Visit The Shop</b>
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={4} >
+          <Grid item xs={4} className="meetsdesk">
             <Card>
               <CardMedia
                 component="img"
-                height="250"
-                image="https://cdn.londonandpartners.com/-/media/images/london/visit/things-to-do/sightseeing/london-attractions/big-ben/big-ben-houses-of-parliament-shutterstock640x360jpg.jpg?h=360&w=640&la=en&hash=AE5224D40454F09CB83D0CE7E8DB02619EEB2AB7"
-                alt="UK"
+                height="350"
+                image={require('../assets/banner.jpg')}
+                alt="Meets"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                  London, England
+                  Meets
                 </Typography>
                 <Typography variant="body1">
-                  Go to England see the Big Ben maybe even have a sip of tea with a flight from FlyDreamAir!
+                  The meets page contains dates and locations of meets being held.
                 </Typography>
-                <Typography color="#2796FD" variant="body2">
-                  <b>Flights from $954</b>
+                <Typography color="#3584e2" variant="body1" component={Link} to="/meets" >
+                  <b>Click Here To See Our Future Meets </b>
                 </Typography>
+                
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={4} >
+          <Grid item xs={12} className="gallerymobile">
             <Card>
               <CardMedia
                 component="img"
-                height="250"
-                image="https://cdn.cheapoguides.com/wp-content/uploads/sites/3/2019/03/Osaka-castle-cherry-blosson-iStock-512491222.jpg"
-                alt="osaka"
+                height="350"
+                image={require('../assets/gallery.jpg')}
+                alt="Gallery"
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                  Osaka, Japan
+                  Gallery
                 </Typography>
                 <Typography variant="body1">
-                  Take in Japan's modern cities and culture. With a trip to Osaka flying FlyDreamAir.
+                  Our gallery contains collections of images of cars that we have shot to display every aspect of people's vechicles.
                 </Typography>
-                <Typography color="#2796FD" variant="body2">
-                  <b>Flights from $1142</b>
+                <Typography color="#3584e2" variant="body1" component={Link} to="/gallery">
+                  <b>Click Here To Visit The Gallery</b>
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
+          <Grid item xs={12} className="shopmobile">
+            <Card>
+              <CardMedia
+                component="img"
+                height="350"
+                image={require('../assets/hoodie.jpg')}
+                alt="Shop"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Shop
+                </Typography>
+                <Typography variant="body1">
+                  Our shop contains high quality clothing, stickers and accessories. 
+                </Typography>
+                <Typography color="#3584e2" variant="body1" component={Link} to="/shop">
+                  <b>Click Here To Visit The Shop</b>
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} className="meetsmobile">
+            <Card>
+              <CardMedia
+                component="img"
+                height="350"
+                image={require('../assets/banner.jpg')}
+                alt="Meets"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Meets
+                </Typography>
+                <Typography variant="body1">
+                  The meets page contains dates and locations of meets being held.
+                </Typography>
+                <Typography color="#3584e2" variant="body1" component={Link} to="/meets" >
+                  <b>Click Here To See Our Future Meets </b>
+                </Typography>
+                
+              </CardContent>
+            </Card>
+          </Grid>
+
         </Grid>
       </Box>
     </Box>
